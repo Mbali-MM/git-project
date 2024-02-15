8ee4dfbe17b606fc263eca8cf35183393ebd69f1
MBALI MNGOMA <144110147+Mbali-MM@users.noreply.github.com>
2024-02-15 14:40:27 +0200

    initial

---
+++ initial.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+
+internal class Program
+{
+    private static void Main(string[] args) => 
+    
+    Console.WriteLine("hello version");
+
+     {
+     Console.WriteLine("hello version2");
+    }
+}
